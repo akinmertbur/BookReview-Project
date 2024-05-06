4210673602623c1fe1e63db39c413f56d9a58009
Akin Mert Bur <akinmertbur@gmail.com>
2024-05-06 15:06:02 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+// Middleware for parsing URL-encoded bodies and extended options.
+app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware to serve static files from the "public" directory.
+app.use(express.static("public"));
+
+// Route for the homepage.
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
