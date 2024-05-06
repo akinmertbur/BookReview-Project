574c02bc427e774b54aef0a55e8130f35b3c1112
Akin Mert Bur <akinmertbur@gmail.com>
2024-05-06 15:49:33 +0300

    connect database & populate with book item for homepage

--- index.js
+++ index.js
@@ -8,14 +8,32 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+// Establish a connection to the database.
+const db = new pg.Client({
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port,
+});
+// Connect to the database.
+db.connect();
+
 // Middleware for parsing URL-encoded bodies and extended options.
 app.use(bodyParser.urlencoded({ extended: true }));
 // Middleware to serve static files from the "public" directory.
 app.use(express.static("public"));
 
+// Initialize an empty array to store books.
+let books = [];
+
 // Route for the homepage.
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM book");
+  books = result.rows;
+  res.render("index.ejs", {
+    bookList: books,
+  });
 });
 
 app.listen(port, () => {
